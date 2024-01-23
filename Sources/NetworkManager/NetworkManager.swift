89b4b74b639e2bc1fb9c65f1064b6bd5e3f356cb
razmaa <145837511+razmaa@users.noreply.github.com>
2024-01-23 16:21:56 +0400

    add package

---
+++ Sources/NetworkManager/NetworkManager.swift
@@ -0,0 +1,43 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+final public class GenericNetworkManager {
+    private let baseURL: String
+    private let endpoint = ""
+    
+    public init(baseURL: String) {
+        self.baseURL = baseURL
+    }
+    
+    public func fetchData<T: Decodable>(endpoint: String, completion: @escaping (Result<T, Error>) -> Void) {
+        let urlString = "\(baseURL + endpoint)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -2, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            do {
+                let dataResponse = try JSONDecoder().decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(dataResponse))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
